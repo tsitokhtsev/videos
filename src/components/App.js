28c2fd63f4068104ca869b98d9b454789a7aed2c
tsitokhtsev <38284633+tsitokhtsev@users.noreply.github.com>
2021-08-09 16:17:27 +0400

    - Updated default search term

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 	state = { videos: [], selectedVideo: null }
 
 	componentDidMount() {
-		this.onTermSubmit('react.js')	
+		this.onTermSubmit('Never Gonna Give You Up')	
 	}
 
 	onTermSubmit = async term => {
