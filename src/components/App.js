b7618aeceeedbfb8595708a873bd9ea255d1817a
tsitokhtsev <tsitokhtsev2002@gmail.com>
2021-08-14 17:51:47 +0400

    Added custom useVideos hook

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,18 @@ import React, { useState, useEffect } from 'react'
 import SearchBar from './SearchBar'
 import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
-import youtube from '../apis/youtube'
+import useVideos from '../hooks/useVideos'
 
 const App = () => {
-	const [videos, setVideos] = useState([])
 	const [selectedVideo, setSelectedVideo] = useState(null)
 
-	useEffect(() => onTermSubmit('Never Gonna Give You Up'), [])
+	const [videos, search] = useVideos('Never Gonna Give You up')
 
-	const onTermSubmit = async term => {
-		const response = await youtube.get('/search', { params: { q: term } })
-
-		setVideos(response.data.items)
-		setSelectedVideo(response.data.items[0])
-	}
-
-	const onVideoSelect = video => setSelectedVideo(video)
+	useEffect(() => setSelectedVideo(videos[0]), [videos])
 
 	return (
 		<div className="ui container">
-			<SearchBar onFormSubmit={onTermSubmit} />
+			<SearchBar onFormSubmit={search} />
 			<div className="ui grid">
 				<div className="ui row">
 					<div className="eleven wide column">
@@ -29,7 +21,7 @@ const App = () => {
 					</div>
 					<div className="five wide column">
 						<VideoList
-							onVideoSelect={onVideoSelect}
+							onVideoSelect={setSelectedVideo}
 							videos={videos}
 						/>
 					</div>
